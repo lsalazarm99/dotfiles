22a5a9a1d731983170a1e68378df35fa7e9ef353
Leonardo Salazar <42286051+lsalazarm99@users.noreply.github.com>
2026-05-29 22:16:46 -0500

    chore: remove fish

--- setup.sh
+++ setup.sh
@@ -17,14 +17,6 @@ echo "⚙️ Setting up package repositories..."
 # Create the keyrings directory if it doesn't exist
 sudo install -dm 755 /etc/apt/keyrings
 
-# Add the fish PPA if it's not already been added
-if ! grep -q "fish-shell" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-  echo "⚙️ Adding fish PPA..."
-  curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x88421e703edc7af54967ded473c9fcc9e2bb48da" | gpg --dearmor | sudo tee /etc/apt/keyrings/shells_fish_release_4.gpg 1>/dev/null
-  echo "deb [signed-by=/etc/apt/keyrings/shells_fish_release_4.gpg] https://ppa.launchpadcontent.net/fish-shell/release-4/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/shells:fish:release:4.list
-  echo "⚙️ fish PPA added."
-fi
-
 # Add the mise PPA if it's not already been added
 if ! grep -q "mise.jdx.dev" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
   echo "⚙️ Adding mise PPA..."
@@ -42,7 +34,7 @@ echo "⚙️ Package list updated."
 
 # Install packages
 echo "⚙️ Installing packages..."
-sudo apt-get -qq install -y bat btop eza fd-find fish fzf mc mise micro ripgrep
+sudo apt-get -qq install -y bat btop eza fd-find fzf mc mise micro ripgrep
 echo "⚙️ Packages installed."
 
 # Add lesspipe.sh if it doesn't exist
@@ -85,16 +77,6 @@ if [[ ! -f /usr/local/bin/aws ]]; then
   else
     echo "⚠️ Unsupported architecture: $ARCH. Skipping AWS CLI installation."
   fi
-fi
-
-# Install fish plugins
-echo "⚙️ Installing fish plugins..."
-fish -c 'curl -fsSL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update </dev/null'
-echo "⚙️ fish plugins installed."
-
-# Make any interactive shell use fish
-if ! grep -q "exec fish" "$HOME"/.bashrc; then
-  echo -e '\nif [[ $- == *i* ]] && [[ -z "$BASH_EXECUTION" ]] && [[ "$SHELL" != */fish ]]; then\n  export BASH_EXECUTION=1\n  exec fish\nfi' >>"$HOME"/.bashrc
 fi
 
 echo "⚙️ Finished setting up your environment!"
